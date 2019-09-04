dba0cea97a099ceefc3525cde676599e16c50100
jacksonmic <jacksonmicheala>
2019-09-03 20:04:07 -0400

    “Asks user for their birth year and prints their Chinese Zodiac symbol.”

--- a01_stubs.py
+++ a01_stubs.py
@@ -25,33 +25,63 @@ print(my_input)
 
 # TODO Check the year using if conditionals, and print the correct animal for that year.
 # See the a01_pets.py for examples
-if my_input == int(1996):
-  print("Your chinese zodiac is a rat")
-if my_input == int(1997):
-   print("Your chinese zodiac is an ox")
 if my_input == int(1998):
-   print("Your chinese zodiac is a tiger")
-if my_input == int(1995):
-   print("Your zodiac is a pig")
-if my_input == int(1994):
-   print("Your zodiac is a dog")
-if my_input == int(1993):
-   print("Your zodiac is rooster")
-if my_input == int(1992):
-   print("Your zodiac is a monkey")
-if my_input == int(1991):
-    print("Your zodiac is a goat")
-if my_input == int(1990):
-    print("Your zodiac is a horse")
-if my_input == int(1989):
-    print("Your zodiac is a snake")
-if my_input == int(1988):
-    print("Your zodiac is a dragon")
+   print("You have the eye of the tiger")
+if my_input == int(1999):
+  print("Look it's Peter Cotton Tail")
+if my_input == int(2000):
+   print("Look it's the year of the dragon")
+if my_input == int(2001):
+   print("You are one sneaky snake")
+if my_input == int(2002):
+   print("You are healthy as a horse")
+if my_input == int(2003):
+   print("Looks like you are the G.O.A.T.")
+if my_input == int(2004):
+   print("monkey see monkey do")
+if my_input == int(2005):
+    print("Only roosters get up early.")
+if my_input == int(2006):
+    print("It's a dog eat dog world")
+if my_input == int(2007):
+    print("feed the piggy bank")
+if my_input == int(2008):
+    print("You eat cheese like a rat")
+if my_input == int(2009):
+    print("You are as strong as an ox")
+if my_input == int(2010):
+    print("you have the eye of the tiger")
 ######################################################################
 # (Required) Task 2
 # TODO Ask the user for their friend's birth year
-
-
+my_input = int(input("Enter your friend's birth year"))
+print(input)
+if my_input == int(1998):
+   print("You have the eye of the tiger")
+if my_input == int(1999):
+  print("Look it's Peter Cotton Tail")
+if my_input == int(2000):
+   print("Look it's the year of the dragon")
+if my_input == int(2001):
+   print("You are one sneaky snake")
+if my_input == int(2002):
+   print("You are as healthy as a horse")
+if my_input == int(2003):
+   print("Looks like you are the G.O.A.T.")
+if my_input == int(2004):
+   print("monkey see monkey do")
+if my_input == int(2005):
+    print("Only roosters get up early.")
+if my_input == int(2006):
+    print("It's a dog eat dog world")
+if my_input == int(2007):
+    print("feed the piggy bank")
+if my_input == int(2008):
+    print("You eat cheese like a rat")
+if my_input == int(2009):
+    print("You are as strong as an ox")
+if my_input == int(2010):
+    print("you have the eye of the tiger")
 # TODO Similar to above, check your friend's year using if conditionals, and print the correct animal for that year
 
 
